5301b04d2dd74b79b7bf0a04dcd5665dd06e7fda
Davi Miranda <davi.miranda@escolar.ifrn.edu.br>
2025-04-10 14:04:12 +0000

    nome e matricula

--- aula_1/olamundo.py
+++ aula_1/olamundo.py
@@ -1,5 +1,4 @@
-a = input()
-if a == "hello":
-    print("o a deu hello")
-else:
-    print("o a é mal educado")
+nome="Francisco Davi Miranda de Oliveira"
+matri="20241011110031"
+
+print(f"Olá, meu nome é {nome}, e minha matrícula é: {matri}")
