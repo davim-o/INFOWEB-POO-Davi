4abaf1c4e4f32a6752140aa19d8b5681602a0b3e
Davi Miranda <davi.miranda@escolar.ifrn.edu.br>
2025-04-22 14:07:16 +0000

    listas

--- Listas de exercicios/lista4.py
+++ Listas de exercicios/lista4.py
@@ -19,3 +19,27 @@ import math
 # else:
 #     print("Nao sao Multiplos")
 
+
+# EXERCÍCIO 4 - DDD
+# ddd=int(input())
+# if ddd==61:
+#     print("Brasilia")
+# elif ddd==71:
+#     print("Salvador")
+# elif ddd==11:
+#     print("Sao Paulo")
+# elif ddd==21:
+#     print("Rio de Janeiro")
+# elif ddd==32:
+#     print("Juiz de Fora")
+# elif ddd==19:
+#     print("Campinas")
+# elif ddd==27:
+#     print("Vitoria")
+# elif ddd==31:
+#     print("Belo Horizonte")
+# else:
+#     print("DDD nao cadastrado")
+
+
+#EXERCÍCIO 5 - Tira-teima
