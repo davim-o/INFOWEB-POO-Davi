da85f07cae3b5430c479caecc261a22a1f2adec1
Davi Miranda <davi.miranda@escolar.ifrn.edu.br>
2025-04-22 13:30:40 +0000

    listas

---
+++ Listas de exercicios/lista4.py
@@ -0,0 +1,21 @@
+import math
+
+#EXERCÍCIO 1 - BHASKARA
+# a, b, c = map(float,input().split())
+# delta= b*b-4*a*c
+# if a==0 or b==0 or c==0 or delta<0:
+#     print("Impossivel calcular")
+# else:   
+#     r1= (-b + math.sqrt((delta)))/(2*a)
+#     r2= (-b - math.sqrt((delta)))/(2*a)
+#     print(f"R1 = {r1:.5f}")
+#     print(f"R2 = {r2:.5f}")
+
+
+#EXERCÍCIO 2 - MULTIPLOS
+# a, b = map(float,input().split())
+# if b%a == 0 or a%b == 0:
+#     print("Sao Multiplos") 
+# else:
+#     print("Nao sao Multiplos")
+
