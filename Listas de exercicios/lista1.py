f6302562d6f9bd56988fd4578eb5311c2e8cdedb
Davi Miranda <davi.miranda@escolar.ifrn.edu.br>
2025-04-22 12:27:53 +0000

    listas

--- Listas de exercicios/lista1.py
+++ Listas de exercicios/lista1.py
@@ -1,4 +1,9 @@
-print("Diga-me seu nome:")
-nome = input()
+#EXERCÍCIO 1
+# nome= "Francisco Davi Miranda de Oliveira"
+# matri="20241011110031"
+# print(f"Seu nome é {nome}, e sua matrícula é {matri}")
 
-print(f"\nBem-vindo(a) ao Codespaces, {nome}")
+#EXRCÍCIO 2
+# print("Diga-me seu nome:")
+# nome = input()
+# print(f"\nBem-vindo(a) ao Codespaces, {nome}
