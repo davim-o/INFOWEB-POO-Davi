8a3842a651086e9be435f3ade445a27478d1d271
Davi Miranda <davi.miranda@escolar.ifrn.edu.br>
2025-09-18 18:55:47 +0000

    Feito

---
+++ Cliente/models/servico.py
@@ -0,0 +1,97 @@
+class Servico:
+    def __init__(self, id,descricao,valor):
+        self.set_id(id)
+        self.set_descricao(descricao)
+        self.set_valor(valor)
+
+
+    def get_id(self): return self.__id
+    def get_descricao(self): return self.__descricao
+    def get_valor(self): return self.__valor
+
+    def set_id(self, id): self.__id = id
+    def set_descricao(self,descricao): self.__descricao = descricao
+    def set_valor(self, valor): self.__valor = valor
+
+
+    def to_json(self):
+        dic1 = {"id":self.__id, "descricao":self.__descricao,
+        "valor":self.__valor}
+        return dic1
+
+    @staticmethod
+    def from_json(dic1):
+        return Servico(dic1["id"], dic1["descricao"], dic1["valor"])
+    
+    def __str__(self):
+        return str(self.__id)
+
+
+
+import json
+
+class servicoDAO():
+    __objetos = []
+    @classmethod
+    def inserir(cls, obj):
+        cls.abrir()
+        id = 0
+
+        for aux in cls.__objetos:
+            if aux.get_id() > id: id = aux.get_id() 
+            obj.set_id(id + 1)
+
+        cls.__objetos.append(obj)
+        cls.salvar()
+
+
+    @classmethod
+    def listar(cls):
+        cls.abrir()
+        return cls.__objetos
+
+    @classmethod
+    def listar_id(cls, id):
+        cls.abrir()
+        for obj in cls.__objetos:
+          if obj.get_id() == id: return obj
+        return None
+    
+
+    @classmethod
+    def atualizar(cls, obj):
+        aux = cls.listar_id(obj.get_id())
+        if aux is not None:
+            cls.__objetos.remove(aux)
+            cls.__objetos.append(obj)
+            cls.salvar()
+
+
+    @classmethod
+    def excluir(cls, obj):
+        aux = cls.listar_id(obj.get_id())
+        if aux is not None:
+            cls.__objetos.remove(aux)
+            cls.salvar()
+
+
+
+    @classmethod
+    def abrir(cls):
+        cls.__objetos = []
+        try:
+            with open("servico.json", mode="r") as arquivo:
+                list_dic1 = json.load(arquivo)
+                for dic1 in list_dic1:
+                    obj = Servico.from_json(dic1)
+                    cls.__objetos.append(obj)
+        except FileNotFoundError:
+            pass
+
+
+    @classmethod
+    def salvar(cls):
+        with open("servico.json", mode="w") as arquivo:
+            json.dump(cls.__objetos, arquivo, default=Servico.to_json)
+
+
--- Listas de exercicios/lista1.py
+++
@@ -1,9 +0,0 @@
-#EXERCÍCIO 1
-# nome= "Francisco Davi Miranda de Oliveira"
-# matri="20241011110031"
-# print(f"Seu nome é {nome}, e sua matrícula é {matri}")
-
-#EXRCÍCIO 2
-# print("Diga-me seu nome:")
-# nome = input()
-# print(f"\nBem-vindo(a) ao Codespaces, {nome}
